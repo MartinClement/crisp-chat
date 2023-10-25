587ea3fe9d61dba92cc12ae478d9d2ca466f94ca
MartinClement <martinclementkjules@gmail.com>
2023-10-25 16:13:01 +0200

    create basic form element

---
+++ src/components/form/BaseInput.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+interface BaseInputProps {
+  name: string;
+  modelValue: string;
+  type?: "text" | "password";
+  valid?: boolean;
+  invalid?: boolean;
+}
+
+const nativeInput = ref<HTMLElement>();
+const isFocus = ref<boolean>(false);
+const forwardClick = () => {
+  console.log("click");
+  nativeInput.value && nativeInput.value.focus();
+};
+const handleFocus = (event: FocusEvent) => {
+  isFocus.value = event.type === "focusin";
+};
+
+const emit = defineEmits<{ (e: "update:modelValue", value: string): void }>();
+const handleInput = (event: Event): void => {
+  const value = (event.target as HTMLInputElement).value;
+  emit("update:modelValue", value);
+};
+
+withDefaults(defineProps<BaseInputProps>(), {
+  type: "text",
+  modelValue: "",
+  valid: false,
+  invalid: false,
+  disabled: false,
+  placeholder: "...",
+});
+</script>
+
+<template>
+  <div
+    class="align-center flex w-full flex-row gap-2 rounded-md border-2 border-solid border-black px-2"
+    @click="forwardClick"
+  >
+    <slot name="before-input"></slot>
+    <input
+      class="borer-none focus:borer-none m-0 grow p-0 focus:outline-none"
+      :id="name"
+      :name="name"
+      :type="type"
+      ref="nativeInput"
+      @focusin="handleFocus"
+      @focusout="handleFocus"
+      @input="handleInput"
+    />
+    <slot name="after-input"></slot>
+  </div>
+</template>
---
+++ src/components/form/BaseLabel.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+export interface BaseLabelProps {
+  target: string;
+}
+defineProps<BaseLabelProps>();
+</script>
+
+<template>
+  <div class="m-0 p-0 text-base text-black">
+    <label :for="target"><slot></slot></label>
+  </div>
+</template>
