3c9718f63b18dd32096481864525b5134d930905
MartinClement <martinclementkjules@gmail.com>
2023-11-01 16:19:22 +0100

    define auth0 creds as env variables

--- src/auth0/auth0.ts
+++ src/auth0/auth0.ts
@@ -2,8 +2,8 @@ import { createAuth0 } from "@auth0/auth0-vue";
 
 // @TODO: Use environment variable instead of hardcoded values
 export default createAuth0({
-  domain: "dev-gavwsrjhlz7odc04.eu.auth0.com",
-  clientId: "Tf5PulyWuJUnha4G6gSPn2osNkYvmS1U",
+  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
   authorizationParams: {
     redirect_uri: window.location.origin,
   },
